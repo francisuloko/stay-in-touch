bac3404a490574a1fd0cb9e59e730b29c71422e9
errea <erreakezy@gmail.com>
2021-10-05 00:03:15 +0100

    Add index to routes for comment resources

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show]
   resources :friendships, only: [:index, :create, :update, :destroy]
   resources :posts, only: [:index, :create] do
-    resources :comments, only: [:create]
+    resources :comments, only: [:index, :create]
     resources :likes, only: [:create, :destroy]
   end
 
